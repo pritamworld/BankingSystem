39bb5d5a5f1ce1faa4ee93b6af53ba4f32bff045
ashishpratheesh <50932031+ashishpratheesh@users.noreply.github.com>
2019-06-02 01:02:07 -0400

    getter and setter added for currentAccount

--- src/main/java/bank/CurrentAccount.java
+++ src/main/java/bank/CurrentAccount.java
@@ -9,7 +9,51 @@ public class CurrentAccount {
     private int insufficentFundCharges;
     private float intrestRate;
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 
+    public float getBalance() {
+        return balance;
+    }
 
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public int getWithDrawalLimit() {
+        return withDrawalLimit;
+    }
+
+    public void setWithDrawalLimit(int withDrawalLimit) {
+        this.withDrawalLimit = withDrawalLimit;
+    }
+
+    public int getTransactionCharges() {
+        return transactionCharges;
+    }
+
+    public void setTransactionCharges(int transactionCharges) {
+        this.transactionCharges = transactionCharges;
+    }
+
+    public int getInsufficentFundCharges() {
+        return insufficentFundCharges;
+    }
+
+    public void setInsufficentFundCharges(int insufficentFundCharges) {
+        this.insufficentFundCharges = insufficentFundCharges;
+    }
+
+    public float getIntrestRate() {
+        return intrestRate;
+    }
+
+    public void setIntrestRate(float intrestRate) {
+        this.intrestRate = intrestRate;
+    }
 }
