6eb42dce56ac8521e2c4d814074b121512ac1f23
richujain <richujain1995@gmail.com>
2019-06-01 21:25:59 -0400

    [Richu] Displaying Account Details Implemented.

--- src/main/java/Executioner/ExecutionerClass.java
+++ src/main/java/Executioner/ExecutionerClass.java
@@ -26,11 +26,18 @@ public class ExecutionerClass {
             System.out.println("7. Deposit");
             Scanner scanner = new Scanner(System.in);
             int id = scanner.nextInt();
+            scanner.nextLine();
             switch (id){
                 case 1 :
                     executionerClass.createBankAccount();
-                    return;
+                    break;
                 case 2 :
+                    System.out.println("Enter Account Number : ");
+                    String accountNumber = scanner.nextLine();
+                    executionerClass.displayAccountDetails(accountNumber);
+                    break;
+                    default:
+                        System.out.println("You have entered an Invalid option. Please try again.");
             }
         }while (choice==0);
 
@@ -57,17 +64,34 @@ public class ExecutionerClass {
         System.out.println("Enter ID Number : ");
         photoAddressId = scanner.nextLine();
         Customer customer = new Customer(personId,name,address,birthDate,contactNumber,emailId,photoAddressId);*/
-        Customer customer = new Customer("asdas","adas","4twt","fghg","asdasd","ertret","asdasd");
-
+        Customer customer = new Customer("0","Richu Jain","84 John Tabor","03/08/1995","+14372391989","richupulimoottil@gmail.com","12/1995/2018");
         customers.add(customer);
-        BankAccount bankAccount = new BankAccount("67267809033","Savings","North York",1200);
+        customer = new Customer("1","Abhishek","Brampton","03/08/1995","+14372391989","richupulimoottil@gmail.com","12/1995/2018");
+        customers.add(customer);
+        customer = new Customer("2","Ashish","Markham Road","03/08/1995","+14372391989","richupulimoottil@gmail.com","12/1995/2018");
+        customers.add(customer);
+        BankAccount bankAccount = new BankAccount("12345","Current","North York",1200);
         bankAccounts.add(bankAccount);
-
-
-
-
+        bankAccount = new BankAccount("12345","Current","North York",2000);
+        bankAccounts.add(bankAccount);
+        bankAccount = new BankAccount("98745","Current","North York",2500);
+        bankAccounts.add(bankAccount);
         System.out.println("Account Created");
 
+    }
+
+    private void displayAccountDetails(String accountNumber){
+        for(int i = 0;i<bankAccounts.size();i++){
+            if(bankAccounts.get(i).getAccountNumber().equals(accountNumber)){
+                System.out.println("Customer ID : "+customers.get(i).getPersonId());
+                System.out.println("Customer Name : "+customers.get(i).getPersonName());
+                System.out.println("Customer Address : "+customers.get(i).getAddress());
+                System.out.println("Customer Date of Birth : "+customers.get(i).getBirthDate());
+                System.out.println("Customer Contact Number : "+customers.get(i).getContactNumber());
+                System.out.println("Customer Email ID : "+customers.get(i).getEmailId());
+                System.out.println("Customer ID Proof Number : "+customers.get(i).getPhotoAddressProofId());
+            }
+        }
     }
 
 
