b7cbb71a55c1f1325f79795f8b9cbaa9af2ee63b
ashishpratheesh <50932031+ashishpratheesh@users.noreply.github.com>
2019-06-02 01:04:28 -0400

    getter and setter added for savingsAccount

--- src/main/java/bank/SavingsAccount.java
+++ src/main/java/bank/SavingsAccount.java
@@ -8,5 +8,43 @@ public class SavingsAccount
     private int transactionCharges;
     private float annualIntrestRate;
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getWithdrawalLimit() {
+        return withdrawalLimit;
+    }
+
+    public void setWithdrawalLimit(int withdrawalLimit) {
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public int getTransactionCharges() {
+        return transactionCharges;
+    }
+
+    public void setTransactionCharges(int transactionCharges) {
+        this.transactionCharges = transactionCharges;
+    }
+
+    public float getAnnualIntrestRate() {
+        return annualIntrestRate;
+    }
+
+    public void setAnnualIntrestRate(float annualIntrestRate) {
+        this.annualIntrestRate = annualIntrestRate;
+    }
 }
