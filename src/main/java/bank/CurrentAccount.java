523a2be1ebffb4bd3970902035c1d8a278eacb7c
ashishpratheesh <50932031+ashishpratheesh@users.noreply.github.com>
2019-06-02 00:48:35 -0400

    declared variables of CurrentAccount class

--- src/main/java/bank/CurrentAccount.java
+++ src/main/java/bank/CurrentAccount.java
@@ -2,4 +2,14 @@ package bank;
 
 public class CurrentAccount {
 
+    private String accountNumber;
+    private int balance;
+    private int withDrawalLimit;
+    private int transactionCharges;
+    private int insufficentFundCharges;
+    private float intrestRate;
+
+
+
+
 }
